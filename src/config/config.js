5832045bd64e01af35a6192e12738d8b8ec34c51
anil babu <anilbabu3245@gmail.com>
2023-10-13 11:02:35 +0530

    change url

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
-export const backendUrl = "http://localhost:8000/";
-export const backendApiUrl = "http://localhost:8000/api/";
+// export const backendUrl = "http://localhost:8000/";
+// export const backendApiUrl = "http://localhost:8000/api/";
 
-// export const backendUrl = 'https://abtechzone.earningappsolutions.online/'
-// export const backendApiUrl = 'https://abtechzone.earningappsolutions.online/api/';
+export const backendUrl = 'https://abtechzone.earningappsolutions.online/'
+export const backendApiUrl = 'https://abtechzone.earningappsolutions.online/api/';
