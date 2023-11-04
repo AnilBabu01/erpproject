80448d92ca7ada73cf4feffd09bc0a09fb85efcf
anil babu <anilbabu3245@gmail.com>
2023-11-05 02:46:42 +0530

    working on coaching

--- src/pages/coaching/frontoffice/enquiry.js
+++ src/pages/coaching/frontoffice/enquiry.js
@@ -21,7 +21,9 @@ import LoadingSpinner from "@/component/loader/LoadingSpinner";
 import moment from "moment";
 import exportFromJSON from "export-from-json";
 import { useReactToPrint } from "react-to-print";
-import { ExportPdfEnquiry } from "../../../component/Coaching/ExportPdf/ExportPdf";
+import jsPDF from "jspdf";
+import "jspdf-autotable";
+import { format } from "date-fns";
 function Enquiry() {
   const componentRef = useRef(null);
   const dispatch = useDispatch();
@@ -141,7 +143,45 @@ function Enquiry() {
   const handlePrint = useReactToPrint({
     content: () => componentRef.current,
   });
-
+  const ExportPdfEnquiry = (isData, fileName) => {
+    const doc = new jsPDF();
+  
+    const tableColumn = [
+      "date",
+      "Name",
+      "Number",
+      "Email",
+      "Address",
+      "Course",
+      "comment",
+    ];
+  
+    const tableRows = [];
+  
+    isData.forEach((item) => {
+      const ticketData = [
+        moment(item?.EnquiryDate).format("MM/DD/YYYY"),
+        item?.StudentName,
+        item?.StudentNumber,
+        item?.StudentEmail,
+        item?.Address,
+        item?.Course,
+        item?.Comment,
+      ];
+  
+      tableRows.push(ticketData);
+    });
+  
+    doc.autoTable(tableColumn, tableRows, { startY: 20 });
+    const date = Date().split(" ");
+  
+    const dateStr = date[0] + date[1] + date[2] + date[3] + date[4];
+  
+    doc.text(`${fileName}`, 8, 9);
+    doc.setFont("Lato-Regular", "normal");
+    doc.setFontSize(28);
+    doc.save(`${fileName}_${dateStr}.pdf`);
+  };
   return (
     <>
       {open && (
