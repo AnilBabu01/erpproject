c238ca314152af18204180573033d6c7da8a791f
anil babu <anilbabu3245@gmail.com>
2023-12-09 10:33:24 +0530

    working on school

--- src/component/MainAdmin/Barchart.js
+++ src/component/MainAdmin/Barchart.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { Bar } from "react-chartjs-2";
+import { BarElement, CategoryScale, LinearScale, Tooltip } from "chart.js";
+import { Chart } from "chart.js";
+Chart.register(CategoryScale, LinearScale, BarElement, Tooltip);
 
-const Barchart = ({ value, pdata }) => {
+export default function Barchart({ value, pdata }) {
   const labels = [
     "April",
     "May",
@@ -17,19 +20,20 @@ const Barchart = ({ value, pdata }) => {
     "March",
   ];
   const allMonths = Array.from({ length: 12 }, (_, index) => index + 1);
-const compareExpensesFeeMonths = (a, b) => {
-  const monthsOrder = [4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2, 3];
+  const compareExpensesFeeMonths = (a, b) => {
+    const monthsOrder = [4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2, 3];
 
-  return monthsOrder.indexOf(a) - monthsOrder.indexOf(b);
-};
+    return monthsOrder.indexOf(a) - monthsOrder.indexOf(b);
+  };
+
   const data = {
     labels: labels,
     datasets: [
       {
         label: value,
-        backgroundColor: "#5ECD07",
-        borderColor: "#5ECD07",
-        data:allMonths?.sort(compareExpensesFeeMonths)?.map((monthNumber) => {
+        backgroundColor: "rgb(255, 99, 132)",
+        borderColor: "rgb(255, 99, 132)",
+        data: allMonths?.sort(compareExpensesFeeMonths)?.map((monthNumber) => {
           // Find the data for the current month
           const dataForMonth = pdata.find(
             (data) => data.MonthNO === monthNumber
@@ -42,11 +46,36 @@ const compareExpensesFeeMonths = (a, b) => {
       },
     ],
   };
-  return (
-    <>
-      <Bar data={data} />
-    </>
-  );
-};
 
-export default Barchart;
+  const options = {
+    plugins: {
+      legend: {
+        display: true,
+        align: "top",
+        labels: {
+          fontColor: "black",
+        },
+      },
+    },
+    scales: {
+      x: {
+        title: {
+          display: true,
+          text: "Month",
+        },
+      },
+      y: {
+        title: {
+          display: true,
+          text: "Amount",
+        },
+        beginAtZero: true,
+      },
+    },
+  };
+  return (
+    <div>
+      <Bar data={data} options={options} />
+    </div>
+  );
+}
--- src/component/MainAdmin/Linechart.js
+++ src/component/MainAdmin/Linechart.js
@@ -1,27 +1,31 @@
 import React from "react";
 import { Line } from "react-chartjs-2";
+import { BarElement, CategoryScale, LinearScale, Tooltip,PointElement,LineElement } from "chart.js";
+import { Chart } from "chart.js";
+Chart.register(CategoryScale, LinearScale, BarElement,PointElement,LineElement, Tooltip);
 
-const labels = [
-  "April",
-  "May",
-  "June",
-  "July",
-  "Aug",
-  "Sep",
-  "Oct",
-  "Nov",
-  "Dec",
-  "Jan",
-  "Feb",
-  "March",
-];
-const allMonths = Array.from({ length: 12 }, (_, index) => index + 1);
-const compareExpensesFeeMonths = (a, b) => {
-  const monthsOrder = [4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2, 3];
+export default function Linechart({ value, pdata }) {
+  const labels = [
+    "April",
+    "May",
+    "June",
+    "July",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+    "Jan",
+    "Feb",
+    "March",
+  ];
+  const allMonths = Array.from({ length: 12 }, (_, index) => index + 1);
+  const compareExpensesFeeMonths = (a, b) => {
+    const monthsOrder = [4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2, 3];
 
-  return monthsOrder.indexOf(a) - monthsOrder.indexOf(b);
-};
-const Linechart = ({ value, pdata }) => {
+    return monthsOrder.indexOf(a) - monthsOrder.indexOf(b);
+  };
+
   const data = {
     labels: labels,
     datasets: [
@@ -42,11 +46,36 @@ const Linechart = ({ value, pdata }) => {
       },
     ],
   };
-  return (
-    <>
-      <Line data={data} />
-    </>
-  );
-};
 
-export default Linechart;
+  const options = {
+    plugins: {
+      legend: {
+        display: true,
+        align: "top",
+        labels: {
+          fontColor: "black",
+        },
+      },
+    },
+    scales: {
+      x: {
+        title: {
+          display: true,
+          text: "Month",
+        },
+      },
+      y: {
+        title: {
+          display: true,
+          text: "Amount",
+        },
+        beginAtZero: true,
+      },
+    },
+  };
+  return (
+    <div>
+      <Line data={data} options={options} />
+    </div>
+  );
+}
